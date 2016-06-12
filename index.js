d1ffb8987db111a9cb6033cb65d4d69db2af43a3
Ouadie Lahdioui <ouadiesoft@gmail.com>
2016-06-12 12:41:30 +0000

    Integrate WitBot with KabourBot

--- index.js
+++ index.js
@@ -24,5 +24,31 @@ if (token) {
 }
 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'You talk to me :heart:')
-})
+    var wit = witbot.process(message.text, bot, message)
+
+    wit.hears('hello', 0.53, function (bot, message, outcome) {
+      bot.startConversation(message, function (_, convo) {
+        convo.say('Hello!')
+        convo.ask('How are you?', function (response, convo) {
+          witbot.process(response.text)
+              .hears('good', 0.5, function (outcome) {
+                convo.say('I am so glad to hear it!')
+                convo.next()
+              })
+              .hears('bad', 0.5, function (outcome) {
+                convo.say('I\'m sorry, that is terrible')
+                convo.next()
+              })
+              .otherwise(function (outcome) {
+                convo.say('I\'m cofused')
+                convo.repeat()
+                convo.next()
+              })
+        })
+      })
+    })
+
+    wit.otherwise(function (bot, message) {
+      bot.reply(message, 'You are so intelligent, and I am so simple. I don\'t understnd')
+    })
+  })
